460c7817db0e76a219aafd722cd91db360ef8014
Enikish <www.1374915762@foxmail.com>
2024-05-30 17:14:30 +0800

    library changes

--- my_library/__manifest__.py
+++ my_library/__manifest__.py
@@ -1,10 +1,10 @@
 {
-    'name': 'My Library',
-    'author': 'Enish',
-    'application': True,
-    'installable': True,
+    'name': 'My library',
+    'author': 'Enish', 
     'version': '17.0.0.1',
+    'application': 'true',
+    'installable': 'true',
+    'data': [],
     'depends': ['base'],
-    'data': [''],
     'license': 'LGPL-3',
 }
